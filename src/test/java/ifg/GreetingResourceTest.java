91fdfceb41efb0538cff8618bd21addb2498a816
Vitaliy Baschlykoff <vitaliy.baschlykoff@mongodb.com>
2025-05-19 11:50:05 +1000

    Add formatter, impsort and yaml config

--- src/test/java/ifg/GreetingResourceTest.java
+++ src/test/java/ifg/GreetingResourceTest.java
@@ -1,20 +1,21 @@
 package ifg;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 class GreetingResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello from Quarkus REST"));
     }
 
 }
